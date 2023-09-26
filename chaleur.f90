34a85a948ab5cd1cf5f98f3c0163d3588f62ad16
Abdoul.Matinou <matinouboni@gmail.com>
2023-09-26 20:59:19 +0200

    vf equation chaleur

---
+++ chaleur.f90
@@ -0,0 +1,16 @@
+program chaleur
+    implicit none
+
+    integer :: n
+    real :: u
+
+    u=1.0
+
+    do n=1,10
+        u=u/2.0+1.0/u
+    end do
+
+    print*, 'approx. de sqrt(2) :',u
+    
+    
+end program chaleur
